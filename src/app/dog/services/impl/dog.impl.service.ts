28bcac7afd4a1e00c9d6a367b8cbe96559dccdc2
dwarakaprasad <dwarka.damodaran@gmail.com>
2020-06-03 10:53:46 -0400

    2 files modified, 1 added, 1 deleted

--- src/app/dog/services/impl/dog.impl.service.ts
+++ src/app/dog/services/impl/dog.impl.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Animal } from "feature-common/service/api/common.interface";
+import { Animal } from "@feature/service/api/common.interface";
 
 @Injectable()
 export class DogService implements Animal {
